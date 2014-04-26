492703960edea0522806eaca133e99205bca850b
Nicklas Ansman Giertz <nicklas@ansman.se>
2014-04-26 23:49:00 +0200

    Add a custom prompt

--- prompt.sh
+++ prompt.sh
@@ -1,2 +1,5 @@
-# h=$(hostname | grep -o "^[^.]\+" | tr "[A-Z]" "[a-z]")
+h=$(hostname | grep -o "^[^.]\+" | tr "[A-Z]" "[a-z]")
 # PROMPT='%{$fg[$NCOLOR]%}%B%n@$h%b%{$reset_color%}:%{$fg[blue]%}%B%c/%b%{$reset_color%} $(git_prompt_info)%(!.#.$) '
+PROMPT='
+%{$fg_bold[green]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt_info) ☃ $h ☃ %{$fg_bold[red]%}%*%{$reset_color%}
+$ '
